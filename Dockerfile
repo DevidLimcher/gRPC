8e92000aaa265c3835edb7612f47e89bd58951b9
DevidLimcher <makmafi@gmail.com>
2024-05-17 11:50:35 +0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use the official Python image from the Docker Hub
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container
+COPY . /app
+
+# Install the dependencies
+RUN pip install -r requirements.txt
+
+# Make port 50051 available to the world outside this container
+EXPOSE 50051
+
+# Run the server
+CMD ["python", "social_network_server.py"]
---
+++ social_network_server.py
@@ -0,0 +1,49 @@
+import os
+from concurrent import futures
+import grpc
+import social_network_pb2
+import social_network_pb2_grpc
+
+class SocialNetworkServicer(social_network_pb2_grpc.SocialNetworkServicer):
+    def __init__(self):
+        self.posts = []
+
+    def CreatePost(self, request, context):
+        post = social_network_pb2.Post(
+            id=str(len(self.posts) + 1),
+            user=request.user,
+            content=request.content,
+            likes=0,
+            comments=[]
+        )
+        self.posts.append(post)
+        return social_network_pb2.CreatePostResponse(post=post)
+
+    def GetFeed(self, request, context):
+        return social_network_pb2.GetFeedResponse(posts=self.posts)
+
+    def LikePost(self, request, context):
+        for post in self.posts:
+            if post.id == request.post_id:
+                post.likes += 1
+                return social_network_pb2.LikePostResponse(post=post)
+        context.abort(grpc.StatusCode.NOT_FOUND, "Post not found")
+
+    def CommentPost(self, request, context):
+        for post in self.posts:
+            if post.id == request.post_id:
+                comment = social_network_pb2.Comment(user=request.user, content=request.content)
+                post.comments.append(comment)
+                return social_network_pb2.CommentPostResponse(post=post)
+        context.abort(grpc.StatusCode.NOT_FOUND, "Post not found")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    social_network_pb2_grpc.add_SocialNetworkServicer_to_server(SocialNetworkServicer(), server)
+    port = os.environ.get('PORT', '50051')
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
